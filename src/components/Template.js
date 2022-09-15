ab60ef981f26215e56e997d17096fa7b2c0ef1d4
Hassen-Ahmed <hassenbest23@gmail.com>
2022-09-14 21:48:36 -0700

    intialise change

--- src/components/Template.js
+++ src/components/Template.js
@@ -14,8 +14,6 @@ function Template(props) {
 
   const [arr, setArr] = useState([]);
 
-  // onClickTask handler
-
   const [currentTime, setCurrentTime] = useState(
     new Date().toLocaleTimeString()
   );
@@ -23,35 +21,45 @@ function Template(props) {
   useEffect(() => {
     setTimeout(() => {
       setCurrentTime(new Date().toLocaleTimeString());
+
       setWhen(new Date().toLocaleDateString());
     }, 1000);
-
-    arr.forEach((item) => {
-      let timeForAlarm = String(item.time.slice(0, 5));
-      if (timeForAlarm[0] === "0") {
-        timeForAlarm = String(item.time.slice(1, 5));
-      } else if (timeForAlarm[0] === "1") {
-        timeForAlarm = String(item.time.slice(0, 5));
-      }
-
-      if (
-        String(timeForAlarm) ===
-        String(currentTime).replace(" PM", "").slice(0, 4)
-      ) {
-        console.log("Bleeping");
-        async function audioHandler() {
-          let audioOne = await new Audio(
-            "http://soundbible.com/grab.php?id=1252&type=mp3"
-          );
-          audioOne.play();
-        }
-
-        audioHandler();
-      }
-    });
-
-    // console.log(String(currentTime).replace(" PM", "").slice(0, 7));
   }, [arr, currentTime]);
+
+  // onClickTask handler
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setCurrentTime(new Date().toLocaleTimeString());
+  //     setWhen(new Date().toLocaleDateString());
+  //   }, 1000);
+
+  //   arr.forEach((item) => {
+  //     let timeForAlarm = String(item.time.slice(0, 5));
+  //     if (timeForAlarm[0] === "0") {
+  //       timeForAlarm = String(item.time.slice(1, 5));
+  //     } else if (timeForAlarm[0] === "1") {
+  //       timeForAlarm = String(item.time.slice(0, 5));
+  //     }
+
+  //     if (
+  //       String(timeForAlarm) ===
+  //       String(currentTime).replace(" PM", "").slice(0, 4)
+  //     ) {
+  //       console.log("Bleeping");
+  //       async function audioHandler() {
+  //         let audioOne = await new Audio(
+  //           "http://soundbible.com/grab.php?id=1252&type=mp3"
+  //         );
+  //         audioOne.play();
+  //       }
+
+  //       audioHandler();
+  //     }
+  //   });
+
+  // console.log(String(currentTime).replace(" PM", "").slice(0, 7));
+  // }, [arr, currentTime]);
 
   const onClickTask = function () {
     if (task.trim() && timeOne.trim() && timeTwo.trim()) {
@@ -138,6 +146,7 @@ function Template(props) {
         onClick={onClickDelete}
         sort={sortHandler}
       />
+      <audio src=""></audio>
     </div>
   );
 }
