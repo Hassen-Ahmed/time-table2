1fdc89db3c5a6b3776c0691c62ccec61b2c568b9
Hassen-Ahmed <hassenbest23@gmail.com>
2022-09-15 18:16:00 -0700

    intialise change

--- src/components/Template.js
+++ src/components/Template.js
@@ -4,9 +4,9 @@ import React, { useState, useEffect } from "react";
 import TimeTable from "./TimeTable";
 import Form from "./Form";
 
-import Bleep from "../audio/Bleep.mp3";
+// import Bleep from "../audio/Bleep.mp3";
 
-import { Howl, Howler } from "howler";
+// import { Howl, Howler } from "howler";
 
 // component setiion down
 
@@ -27,40 +27,40 @@ function Template(props) {
     new Date().toLocaleTimeString()
   );
 
-  const [sound] = useState(
-    new Howl({
-      src: Bleep,
-    })
-  );
+  // const [sound] = useState(
+  //   new Howl({
+  //     src: Bleep,
+  //   })
+  // );
   //
-  Howler.volume(0.5);
+  // Howler.volume(0.5);
 
   useEffect(() => {
-    const soundPlay = () => sound.play();
+    // const soundPlay = () => sound.play();
 
     setTimeout(() => {
       setCurrentTime(new Date().toLocaleTimeString());
       setWhen(new Date().toLocaleDateString());
     }, 1000);
 
-    arr.forEach((item) => {
-      let timeForAlarm = String(item.time.slice(0, 5));
-      if (timeForAlarm[0] === "0") {
-        timeForAlarm = String(item.time.slice(1, 5));
-      } else if (timeForAlarm[0] === "1") {
-        timeForAlarm = String(item.time.slice(0, 5));
-      }
+    // arr.forEach((item) => {
+    //   let timeForAlarm = String(item.time.slice(0, 5));
+    //   if (timeForAlarm[0] === "0") {
+    //     timeForAlarm = String(item.time.slice(1, 5));
+    //   } else if (timeForAlarm[0] === "1") {
+    //     timeForAlarm = String(item.time.slice(0, 5));
+    //   }
 
-      if (
-        String(timeForAlarm) ===
-        String(currentTime).replace(" PM", "").slice(0, 4)
-      ) {
-        soundPlay();
-      }
-    });
+    //   if (
+    //     String(timeForAlarm) ===
+    //     String(currentTime).replace(" PM", "").slice(0, 4)
+    //   ) {
+    //     // soundPlay();
+    //   }
+    // });
 
     // console.log(String(currentTime).replace(" PM", "").slice(0, 7));
-  }, [arr, currentTime, sound]);
+  }, [arr, currentTime]);
 
   // onClickTask handler
 
